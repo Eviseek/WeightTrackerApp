8eaa65397c37ed71f0e9593ad849be06f0c220ce
Eva Chlpikova <chlpikova.e@gmail.com>
2024-01-27 19:14:10 +0100

    Initial Commit

---
+++ WeightTracker/WeightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightTrackerApp.swift
+//  WeightTracker
+//
+//  Created by Eva Chlpikova on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
