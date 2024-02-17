4462e12bc2e41595243eac3acaf1e598137b4d63
Eva Chlpikova <chlpikova.e@gmail.com>
2024-02-17 19:11:15 +0100

    almost finished the HealthKit integration - added writing, UI handling etc. added Unit tests for WeightDataHandler class and HealthManager

---
+++ WeightTracker/View/MainScreen/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeightTracker
+//
+//  Created by Eva Chlpikova on 17.02.2024.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
